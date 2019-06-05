066ef23fc71bc8b8621bbbdd28c168cfe39ca55c
ArtemAlagizov <alagizov@gmail.com>
2019-06-05 11:11:02 +0200

    added Swagger documentation for the model

--- src/main/java/com/spring/boot/template/model/Data.java
+++ src/main/java/com/spring/boot/template/model/Data.java
@@ -1,12 +1,16 @@
 package com.spring.boot.template.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@ApiModel(description = "All details about the data.")
 public class Data {
     @Setter
     @Getter
     @JsonProperty
+    @ApiModelProperty(notes = "The very important value")
     private Integer value;
 }
